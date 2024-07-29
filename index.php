5f6e68a5ad6d4b8fa8a910a52c0dbf6c2514288f
PiyushchandraVyas <piyushvyas105@gmail.com>
2024-07-29 13:03:00 +0530

    Add files via upload
    done

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+   #bbq {
+    display: flex;
+    
+    
+   }
+
+   .card {
+    margin-top: 5%;
+    margin-left: 5%;
+   }
+
+   .a2{
+    background-color: aqua;
+
+   }
+
+   .utube {
+    margin-left: 5%;
+    margin-top: 5%;
+   }
+
+   .maps {
+    margin-top: 5%;
+    margin-left: 5%;
+   }
+
+   .carousel {
+    width: 70%;
+    margin-top: 5%;
+   }
+
+   .d-block {
+    height: 500px;
+   }
+   
+   .img1{
+
+    height:   ;
+    width:;
+   } 
+
+
+   .footer-content p {
+    margin: 0;
+}
+
+.footer-content ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+.footer-content ul li {
+    display: inline-block;
+    margin-right: 10px;
+}
+
+.footer-content ul li a {
+    color: #fff;
+    text-decoration: none;
+}
+
+.footer-content ul li a:hover {
+    text-decoration: underline;
+}
+
+
+
+
+    </style>
+
+
+
+</head>
+<body>
+<div style="background-color:beige;">
+    <div class="container">
+       <center><h1 class="a1" style="color: black; font-style:italic;">KAFFEEKLATSCH</h1></center>
+    </div>
+
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <!-- navbar end -->
+
+          <center>
+
+          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://cdn.pixabay.com/photo/2017/03/27/14/21/chairs-2179044_640.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://cdn.pixabay.com/photo/2016/11/23/14/44/delicious-1853300_640.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://cdn.pixabay.com/photo/2015/06/30/18/36/st-826688_640.jpg" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+                   </center>
+          <!-- carousel end -->
+
+
+         
+
+         <br> <center><h3>Welcome!</h3></center>
+         <center><i><h3>Indulge yourself in your Flavor Fantasies</h3></center></i>
+
+         <center>
+
+          <div id="bbq">
+
+            <div class="card" style="width: 18rem;">
+                <img src="https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Food</h5>
+                  <p class="card-text">Experience a symphony of flavors with every bite—our dishes are crafted to delight your palate and elevate your dining experience.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+    
+              <div class="card" style="width: 18rem;">
+                <img src="https://plus.unsplash.com/premium_photo-1664530452529-60020787050a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwcmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fHww" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Atmosphere</h5>
+                  <p class="card-text">Immerse yourself in our warm and inviting atmosphere, where elegant decor and soft lighting create the perfect backdrop for a memorable dining experience.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+    
+              <div class="card" style="width: 18rem;">
+                <img src="https://images.unsplash.com/photo-1551632436-cbf8dd35adfa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhhcHB1eSUyMHJlc3RhdXJhbnQlMjBzdGFmZnxlbnwwfHwwfHx8MA%3D%3D" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Hygiene and Service</h5>
+                  <p class="card-text">Enjoy peace of mind with our unwavering commitment to cleanliness and impeccable service, ensuring every visit is both hygienic and exceptional.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+
+
+          </div>
+
+        </center>
+    
+          <!-- card end -->  
+           
+          <br>
+          <center><h4>More About Us</h4></center>
+
+          <div class="utube">
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/8oWhOgFS0eU?si=HK1ueFTDSZNe8cbr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/XQjcDyWqLY4?si=nYzRhZDtpF4B2AaC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+
+          <!-- utube end -->
+            
+          <br>
+          <center><h4 style="color: cornflowerblue;">Know Our Location</h4></center>
+          <div class="maps">
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15051260.487269107!2d63.30272238600081!3d22.927230129017495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd31be8050c3%3A0x396a2ac036490cb8!2sThe%20Lalit%20New%20Delhi!5e0!3m2!1sen!2sin!4v1721987715476!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+          </div>
+
+          <!-- maps end -->
+
+          <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <div class="container-fluid" style="justify-content: center;">
+                <a class="navbar-brand" href="#"></a>
+              </div>
+            </nav>
+          </div>
+          
+
+</div>
+
+
+<center><h1>Contact Us</h1></center>
+<br>
+<form action="form.php"method="POST">
+
+<label for="name">Enter Name</label>
+<br>
+<input type="text" id="name" name="name">
+<br>
+
+<label for="mobile">Enter Mobile No.</label>
+<br>
+<input type="number" name="mobile" id="mobile">
+<br>
+<label for="email">Enter Email Address</label>
+<br>
+<input type="text" name="email" id="email">
+
+
+<br><br>
+<input type="submit" value="submit">
+</form>
+
+
+
+ <footer>
+    <div class="footer-content">
+        <p>&copy; 2024 Kaffeeklatsch, All rights reservewd. </p>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+ </footer>
+
+    
+
+
+
+
+  
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+</html>
